7b141ac16d32ebe1115fc5bfae899d230aa2f1de
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-03-04 01:05:30 +0800

    Update board.h

--- trunk/configs/boards/WR330/board.h
+++ trunk/configs/boards/WR330/board.h
@@ -1,34 +1,33 @@
-/* YOUHUA WR1200JS */
+/* YOUHUA WR330 */
 
-#define BOARD_PID		"WR1200JS"
-#define BOARD_NAME		"WR1200JS"
-#define BOARD_DESC		"YOUHUA WR1200JS Wireless Router"
+#define BOARD_PID		"WR330"
+#define BOARD_NAME		"WR330"
+#define BOARD_DESC		"YOUHUA WR330 Wireless Router"
 #define BOARD_VENDOR_NAME	"Youhua Technology"
 #define BOARD_VENDOR_URL	"http://www.youhuatech.com/"
-#define BOARD_MODEL_URL		"http://www.youhuatech.com/"
-#define BOARD_BOOT_TIME		20
+#define BOARD_MODEL_URL	"http://www.youhuatech.com/"
+#define BOARD_BOOT_TIME	30
 #define BOARD_FLASH_TIME	120
-#undef BOARD_GPIO_BTN_FN1
-#undef BOARD_GPIO_BTN_RESET  
-#define BOARD_GPIO_BTN_WPS	 3
+#define BOARD_GPIO_BTN_FN1	18	//WiFi
+#define BOARD_GPIO_BTN_RESET	3
+#define BOARD_GPIO_BTN_WPS	12
 #undef  BOARD_GPIO_LED_ALL
-#define  BOARD_GPIO_LED_WIFI	14
+#undef  BOARD_GPIO_LED_WIFI
 #undef  BOARD_GPIO_LED_SW2G
 #undef  BOARD_GPIO_LED_SW5G
-#define  BOARD_GPIO_LED_POWER	 12
+#undef  BOARD_GPIO_LED_POWER
 #undef  BOARD_GPIO_LED_LAN
-#define BOARD_GPIO_LED_WAN	16
-#undef BOARD_GPIO_LED_USB
-#undef BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_LED_WAN	6
+#define BOARD_GPIO_LED_USB	8
+#define BOARD_GPIO_LED_ROUTER	7	//WPS
 #undef  BOARD_GPIO_PWR_USB
 #define BOARD_HAS_5G_11AC	0
 #define BOARD_NUM_ANT_5G_TX	0
 #define BOARD_NUM_ANT_5G_RX	0
 #define BOARD_NUM_ANT_2G_TX	0
 #define BOARD_NUM_ANT_2G_RX	0
-#define BOARD_NUM_ETH_LEDS	0
-#define BOARD_NUM_ETH_EPHY	4
+#define BOARD_NUM_ETH_LEDS	1
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
 #define BOARD_NUM_UPHY_USB3	0
-#define BOARD_USB_PORT_SWAP	0
+#define BOARD_USB_PORT_SWAP	1
